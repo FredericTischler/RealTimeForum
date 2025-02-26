41b8ffd12d673272b25e525592f9ba4af721b601
ftischle <frederic.tischler@protonmail.com>
2025-02-26 16:27:00 +0100

    users response body

--- models/users.go
+++ models/users.go
@@ -16,3 +16,21 @@ type User struct {
 	gender    string
 	createdAt time.Time
 }
+
+type RegisterResponse struct {
+	status  int
+	message string
+	userId  uuid.UUID
+}
+
+type LoginResponse struct {
+	status  int
+	message string
+	userId  uuid.UUID
+	token   string
+}
+
+type LogoutResponse struct {
+	status  int
+	message string
+}
