366f4ab1a6c2fe17cb68709bdec7e9e49e4d6e77
ftischle <frederic.tischler@protonmail.com>
2025-02-26 16:01:26 +0100

    auth routes

--- config/routes.go
+++ config/routes.go
@@ -7,4 +7,8 @@ import (
 
 func InitializeRoutes(mux *http.ServeMux) {
 	mux.HandleFunc("/", handlers.HomeHandler)
+	mux.HandleFunc("POST /register", handlers.RegisterHandler)
+	mux.HandleFunc("POST /login", handlers.LoginHandler)
+	mux.HandleFunc("POST /logout", handlers.LogoutHandler)
+
 }
--- handlers/auth_handler.go
+++ handlers/auth_handler.go
@@ -1,1 +1,15 @@
 package handlers
+
+import "net/http"
+
+func RegisterHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func LogoutHandler(w http.ResponseWriter, r *http.Request) {
+
+}
