aab6fd59606f4cb02ee34f852205d3c02b67bbc5
ftischle <frederic.tischler@protonmail.com>
2025-03-04 09:40:47 +0100

    posts service

--- services/post_service.go
+++ services/post_service.go
@@ -1,1 +1,27 @@
 package services
+
+import (
+	"forum/models"
+	"forum/repositories"
+
+	"github.com/gofrs/uuid"
+)
+
+type PostsService struct {
+	PostsRepo *repositories.PostsRepository
+}
+
+// InsertPost appelle le repository pour insérer un post.
+func (ps *PostsService) InsertPost(userId uuid.UUID, title, content, category string, postUUID uuid.UUID) (int64, error) {
+	return ps.PostsRepo.InsertPost(userId, title, content, category, postUUID)
+}
+
+// GetPostById retourne un post en fonction de son UUID.
+func (ps *PostsService) GetPostById(postUUID uuid.UUID) (*models.Post, error) {
+	return ps.PostsRepo.GetPostById(postUUID)
+}
+
+// GetAllPosts retourne la liste de tous les posts.
+func (ps *PostsService) GetAllPosts() ([]*models.Post, error) {
+	return ps.PostsRepo.GetAllPosts()
+}
